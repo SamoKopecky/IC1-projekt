988b593aad2a23c9b60632b0f9a5299139e6b077
Samuel Kopecký <xkopec52@vutbr.cz>
2020-03-09 14:57:52 +0100

    Send commands over newtork and show the output

--- virtual_cmd/client_side.py
+++ virtual_cmd/client_side.py
@@ -3,5 +3,9 @@ import sys
 sys.path.insert(1, '../')
 from virtual_cmd.communication import User as user
 
+# example netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
+
 user = user.use_user('send')
-user.send_data(input())
+command = input("enter command : ")
+user.send_data(command)
+print(user.receive_data())
--- virtual_cmd/server_side.py
+++ virtual_cmd/server_side.py
@@ -1,8 +1,9 @@
 import sys
+import subprocess
 
 sys.path.insert(1, '../')
-from utils import execute_cmd_command as exe_cmd
 from virtual_cmd.communication import User as user
 
 user = user.use_user('receive')
-print(user.receive_data())
+received_command = user.receive_data()
+user.send_data(subprocess.getoutput(received_command))
